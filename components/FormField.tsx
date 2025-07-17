b25f82e98928b91c88bf66fd982a6f43d765b697
ShahriyarSuhan <shahriyarsuhan@gmail.com>
2025-07-18 02:29:53 +0600

    setup files, projects and folder structure

---
+++ components/FormField.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Input } from './ui/input'
+import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from './ui/form'
+import { Control, Controller, FieldValues, Path } from 'react-hook-form'
+
+interface FormFieldProps<T extends FieldValues> {
+    control: Control<T>;
+    name: Path<T>;
+    label: string;
+    placeholder?: string;
+    type?: "text" | "email" | "password" | "file";
+}
+
+const FormField = ({ name, control, label, placeholder, type = "text"}: FormFieldProps<T>) => (
+    <Controller 
+    control={control} 
+    name={name} 
+    render={({ field }) => (
+        <FormItem>
+            <FormLabel className="label">{label}</FormLabel>
+            <FormControl>
+                <Input className="input" placeholder={placeholder} type={type} {...field} />
+            </FormControl>
+            <FormMessage />
+        </FormItem>
+    )}
+    />
+)
+
+export default FormField
