9821871ae24c014f9a6284b0ba63a702e8243729
ShahriyarSuhan <shahriyarsuhan@gmail.com>
2025-07-29 01:23:07 +0600

    interview generation ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+function Page() {
+  return (
+    <>
+    <h3>Interview Screen</h3>
+    <Agent userName="You" userId="user1" type="generate"/>
+    </>
+  )
+}
+
+export default Page
